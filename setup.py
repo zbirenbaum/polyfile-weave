d46cf59ad62123dfd511953bea6c76d4e86b54cb
zbirenbaum <zachary.birenbaum@wandb.com>
2025-07-28 13:25:17 -0700

    fix version error

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     long_description_content_type="text/markdown",
     url='https://github.com/zbirenbaum/polyfile-weave',
     author='Trail of Bits',
-    version="0.5.5",
+    version="0.5.6",
     packages=find_packages(exclude=("tests",)),
     python_requires='>=3.8',
     install_requires=[
